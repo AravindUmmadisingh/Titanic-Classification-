34c5825d77977cf27754df8e7e658ea4d665c1bf
AravindUmmadisingh <158826044+AravindUmmadisingh@users.noreply.github.com>
2024-06-23 11:38:45 +0530

    Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -4,20 +4,18 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 plt.style.use('fivethirtyeight') 
-%matplotlib inline 
 warnings.filterwarnings('ignore') 
 train = pd.read_csv('train.csv') 
 test = pd.read_csv('test.csv') 
 train.shape 
-# (891, 12) 
 train.info() 
 train.isnull().sum() 
 f, ax = plt.subplots(1, 2, figsize=(12, 4)) 
 train['Survived'].value_counts().plot.pie( 
-	explode=[0, 0.1], autopct='%1.1f%%', ax=ax[0], shadow=False) 
+   explode=[0, 0.1], autopct='%1.1f%%', ax=ax[0], shadow=False) 
 ax[0].set_title('Survivors (1) and the dead (0)') 
 ax[0].set_ylabel('') 
-sns.countplot('Survived', data=train, ax=ax[1]) 
+sns.countplot(x='Survived', data=train, ax=ax[1]) 
 ax[1].set_ylabel('Quantity') 
 ax[1].set_title('Survivors (1) and the dead (0)') 
 plt.show() 
